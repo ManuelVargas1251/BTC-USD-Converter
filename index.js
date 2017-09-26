b33450312770f5430aa47b1fdf6498ce1e0369c1
Manuel Vargas <manuelvargas@my.unt.edu>
2017-09-26 01:47:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,42 @@
 
 // Create a new module
 console.clear();
-var i = 0;
-//var btc_json = "https://api.coindesk.com/v1/bpi/currentprice.json";
+var api_url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 var btc_app = angular.module('btc_app',[]);
 	
 btc_app.controller('price', function ($scope){
-	//var current_btc = 5;
-	console.log("controller!");
+	console.log("-- price controller --");
 	
-	$.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json", function(data){
+	$.getJSON(api_url, function(data){
 		var current_btc = data.bpi.USD.rate_float;
 		console.log(current_btc);
-		//console.log(i++ + ": btc: " + "g");
 		$scope.btc = current_btc;
-		$scope.$apply();
+		$scope.$apply();	//vital piece of the puzzle
 	});
-	
-	//var my = current_btc;
-	//	console.log("current_btc: "+ my);
-	
-	
-	//current_btc = 9;
+	//console.log("wa " + get_price());
 });
+
+
+//update price
+$('#update_price').click(function(){
+	console.log("update price");
+	
+	$.getJSON(api_url, function(data){
+		var current_btc = data.bpi.USD.rate_float;
+		console.log(current_btc);
+		$scope.btc = current_btc;
+		$scope.$apply();	//vital piece of the puzzle
+	});
+});
+
+//round toggle
+$('#round_toggle').click(function(){
+	console.log("round price");
+	//get_price();
+});
+
+//$('#button').css
+
+//cursor blinking
+//$("#focus").focus();
